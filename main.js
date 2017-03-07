3919167e04e43e96d3a20839b6b6c656b60d9e3f
Zoe Ames <zoeames@gmail.com>
2017-03-06 19:58:17 -0600

    added xhr request

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello");
+
+function executeThisCodeAfterFileLoaded(){
+	console.log("It WORKED!!!");
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("booooooo");
+}
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
+
+
+
+
+
+
+
+
+
+
